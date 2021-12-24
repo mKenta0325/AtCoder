9ebc51f7414540fc2eeb3915ed449496465e13d9
kinutaISE <nknopt.ise@gmail.com>
2021-12-24 22:06:27 +0900

    minor change

--- typicalProblems90/No.8_AtCounter/AtCounter.cpp
+++ typicalProblems90/No.8_AtCounter/AtCounter.cpp
@@ -21,20 +21,37 @@ const string ATCODER = "atcoder" ;
 vector< vector<int> > table ;
 
 void memoized_recursion_core(int i, int k) {
-  if (i == 0) {
-    table[0][k] = 0 ;
+  if (k == 0) {
+    table.at(i).at(0) = 1 ;
     return ;
   }
-  int unused_num = table.at(i - 1).at(k) ;
-  if (unused_num == -1) {
-    memoized_recursion_core(i - 1, k) ;
-    unused_num = table.at(i - 1).at(k) ;
+  if (i < k) {
+    table.at(0).at(k) = 0 ;
+    return ;
   }
 
-  // ここから！
+  int ip = i - 1 ;
+
+  int unused_num = table.at(ip).at(k) ; // i 番目の文字を用いない場合の総数
+  if (unused_num == -1) {
+    memoized_recursion_core(ip, k) ;
+    unused_num = table.at(ip).at(k) ;
+  }
+  table.at(i).at(k) = unused_num ;
+
+  int kp = k - 1 ;
+  if (str.at(ip) != ATCODER.at(kp))
+    return ;
+
+  int used_num = table.at(ip).at(kp) ;
+  if (used_num == -1) {
+    memoized_recursion_core(ip, kp) ;
+    used_num = table.at(ip).at(kp) ;
+  }
+  table.at(i).at(k) += used_num ;
 }
 void memoized_recursion() {
-  table = vector< vector<int> >(vector<int>(str_length + 1), ATCODER.size() + 1) ;
+  table = vector< vector<int> >( str_length + 1, vector<int>(ATCODER.size() + 1) ) ;
   for (int i = 0 ; i <= str_length ; i++) {
     for (int k = 0 ; k <= ATCODER.size() ; k++)
       table.at(i).at(k) = -1 ;
@@ -46,7 +63,9 @@ int main() {
   cin >> str_length ;
   cin >> str ;
 
+  memoized_recursion() ;
 
+  cout << table.at(str_length).at(ATCODER.size()) << endl ;
 
   return 0 ;
 }
