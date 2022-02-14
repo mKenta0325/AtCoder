ac3d77c55f979f8e7863a238f7f6686abe9108f6
kinutaISE <nknopt.ise@gmail.com>
2022-02-14 15:18:25 +0900

    Create main.py

---
+++ Python/contests/AHC008/main.py
@@ -0,0 +1,11 @@
+class Agent:
+    def __init__(self, i, x, y):
+        self.index = i
+        self.pos_x = x
+        self.pos_y = y
+class Pet:
+    def __init__(self, i, x, y, t):
+        self.index = i
+        self.pos_x = x
+        self.pos_y = y
+        self.type = t
