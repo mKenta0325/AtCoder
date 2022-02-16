8acc21fded5bf518021da57e2c695077b6223a63
kinutaISE <nknopt.ise@gmail.com>
2022-02-16 18:13:06 +0900

    minor change

---
+++ Coursera/Java Programming and Software Engineering Fundamentals/Programming Foundations with JavaScript, HTML and CSS/Week3/Canvas/Canvas.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  var canElement1 = document.getElementById("can1") ;
+  var canElement2 = document.getElementById("can2") ;
+  // canElement1.className = "blueback" ;
+  canElement1.style.backgroundColor = "blue" ;
+  // canElement2.className = "orangeback" ;
+  canElement2.style.backgroundColor = "orange" ;
+}
